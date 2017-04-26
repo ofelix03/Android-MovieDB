48e69556810dc184caeae3cd229a90deeff54544
Felix Otoo <ofelix03@gmail.com>
2017-04-26 10:31:32 +0000

    Add interfaces

---
+++ app/src/main/java/com/felix/moviedb/moviedb/interfaces/MovieViewHolderCallback.java
@@ -0,0 +1,11 @@
+package com.felix.moviedb.moviedb.interfaces;
+
+import android.view.View;
+
+/**
+ * Created by felix on 1/22/17.
+ */
+
+public interface MovieViewHolderCallback {
+    public void onImageClick(View view, int tvId);
+}
